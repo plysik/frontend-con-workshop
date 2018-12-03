f28bff77795f037e5ee2e4f6a6a3adb1e2e1ac0d
Piotr Łysik <piotrek.lysik@gmail.com>
2018-12-03 13:25:26 +0100

    Updates

---
+++ precache-manifest.44d3913615cb74319d6dd78599f58b6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d2324a70a34623daf41",
+    "url": "/frontend-con-workshop/static/js/runtime~main.2d2324a7.js"
+  },
+  {
+    "revision": "edb2556273061d294b79",
+    "url": "/frontend-con-workshop/static/js/main.edb25562.chunk.js"
+  },
+  {
+    "revision": "efbefe8e7691eefbfce0",
+    "url": "/frontend-con-workshop/static/js/1.efbefe8e.chunk.js"
+  },
+  {
+    "revision": "edb2556273061d294b79",
+    "url": "/frontend-con-workshop/static/css/main.1702ce1f.chunk.css"
+  },
+  {
+    "revision": "18d22ea1225c742a34707a16f1f0098e",
+    "url": "/frontend-con-workshop/index.html"
+  }
+];
